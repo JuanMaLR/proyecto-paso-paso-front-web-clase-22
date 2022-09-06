c2a95d2dbdd890e65e92868753a55e78c89f269e
JuanMaLR <A01206568@itesm.mx>
2022-09-06 14:24:57 -0500

    Práctica 2 completada

---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop';
+import Footer from './Footer';
+import Table from './tableComponents/Table';
+
+function ContentWrapper(){
+    return (
+        <React.Fragment>
+            {/*<!-- Content Wrapper -->*/}
+            <div id="content-wrapper" className="d-flex flex-column">
+                {/*<!-- Main Content -->*/}
+                <div id="content">
+                    <TopBar />
+                    <ContentRowTop />
+                </div>
+                <Table/>
+                <Footer />
+            </div>    
+        </React.Fragment>
+    )
+}
+export default ContentWrapper;
